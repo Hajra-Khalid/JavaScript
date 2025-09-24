17e67e441b442e51d5955c2773a5f39121acf6e7
Hajra-Khalid <hajraqureshi629@gmail.com>
2025-09-24 16:56:43 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEMail = "hajraqureshi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+// accountId = 2
+console.log(accountId);
+
+/*
+prefere not touse var because of issue in blockscop and functional scop
+*/
+
+
+
+accountEmail = "hajar@hajra.com";
+accountPassword = "12121212"
+accoutnCity = "Hyderabad" 
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId , accountEmail , accountPassword , accountCity]);
